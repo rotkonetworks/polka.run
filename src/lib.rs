3c74b7a488a27d56801be593e5bfeea170871d3c
Daniel Rosengren <9402+d-r@users.noreply.github.com>
2024-01-11 00:25:13 +0100

    Remove unnecessary #[path] attributes

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
-#[path = "app.rs"]
 pub mod app;
-
-#[path = "navigation.rs"]
 pub mod navigation;
-
-#[path = "home.rs"]
 pub mod home;
-
-#[path = "disassembler.rs"]
 pub mod disassembler;
-
-#[path = "file_upload.rs"]
 pub mod file_upload;
